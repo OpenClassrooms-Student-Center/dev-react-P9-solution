0cfdfd55afc2c133d3387e9b4ea11502ab7cc334
Alexia Toulmet <alexia.toulmet@gmail.com>
2025-08-01 15:59:00 +0200

    update ts-config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
+// Configuration TypeScript pour ignorer l'erreur de type à supprimer quand App sera en TypeScript
 // @ts-expect-error
 import App from "./components/App";
 
